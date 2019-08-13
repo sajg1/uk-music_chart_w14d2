9797df16fc93f6da378375c4208a584a360054fe
Steve Gossip <stephengossip@gmail.com>
2019-08-13 16:02:16 +0100

    Component Did Mount is making the API fetch and removing some of the head of the object to leave array with 20 entries fro songs.

--- src/containers/MusicBox.js
+++ src/containers/MusicBox.js
@@ -2,6 +2,24 @@ import React, {Component} from 'react';
 import ChartList from '../components/ChartList'
 
 class MusicBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ukChart: []
+    }
+  }
+
+  componentDidMount() {
+    const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json'
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        const ukChart = data.feed.entry;
+        this.setState({ukChart})
+      })
+      .catch(err => console.err)
+  }
+
   render(){
     return(
       <div className="music-box">
